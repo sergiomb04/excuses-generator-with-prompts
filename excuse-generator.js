93386b34ed3a4cbec3fe2cc271b37a9c1e742580
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2026-03-09 16:02:33 +0100

    step 2: Crear arreglo 'who'

--- excuse-generator.js
+++ excuse-generator.js
@@ -1,3 +1,11 @@
 // Excuse Generator - A random excuse generator for procrastinators
 
+// Array of possible subjects that can start an excuse
+const who = [
+  "My dog",
+  "My cat",
+  "My boss",
+  "My friend"
+];
+
 console.log("Excuse Generator is ready to run!");
